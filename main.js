331a9b0e8cdcd3f9a29822b7c6ca93526080e766
Teu Pai <105930953+Aluninho07@users.noreply.github.com>
2024-04-16 11:04:24 -0300

    sda

--- main.js
+++ main.js
@@ -21,6 +21,13 @@ function calculaTempo(tempoObjetivo1){
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
 
-return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+        segundos %= 60;
+        minutos %= 60;
+        horas %= 24;
+
+  return dias + "dias" 
+    + horas + "horas" 
+    + minutos + "minutos" 
+    + segundos + "segundos";
 }
 
