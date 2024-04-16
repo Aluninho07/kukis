4edf5c86e3a94bb3e9a86f094b8f386c7b83889f
Teu Pai <105930953+Aluninho07@users.noreply.github.com>
2024-04-16 10:48:15 -0300

    a

--- main.js
+++ main.js
@@ -14,6 +14,6 @@ for (let i = 0; i < botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-01T00:00:00");
 let tempoAtual = new Date();
-function calculaTempo(tempoObjetivo)
+function calculaTempo(tempoObjetivo1)
 contadores[0].textContent = "Contagem Regressiva";
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
